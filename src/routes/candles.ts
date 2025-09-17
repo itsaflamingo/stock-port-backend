6fc7a5edc8796b8c3039242d702c41681669eab4
elizabeth pinero <elizabeth.pinero.r@gmail.com>
2025-09-17 13:21:56 +0000

    added candles functionality, can now send stock prices for period at specified intervals to frontend

--- src/routes/candles.ts
+++ src/routes/candles.ts
@@ -1,9 +1,37 @@
 import express from "express"
-import fetch from "node-fetch"
+import yahooFinance from "yahoo-finance2"
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", async (req, res) => {
+    let candles;
+    // Get historical data from Yahoo Finance from start to end, representing different candles
+    try {
+        const result = await yahooFinance.chart(req.body.symbol, {
+            period1: req.body.period1,
+            period2: req.body.period2,
+            interval: req.body.interval
+        })
+
+        candles = result.quotes.map(q => ({
+            date: q.date,
+            open: q.open,
+            high: q.high,
+            low: q.low,
+            close: q.close,
+            volume: q.volume,
+        }));
+    } catch (error: any) {
+        console.error(error);
+        // Inspect error and decide what to do; often, you may want to just abort:
+        console.warn(
+            `Skipping yf.quote("${req.body.symbol}"): [${error.name}] ${error.message}`,
+        );
+        return;
+    }
+
+    // send candles
+    res.send(candles)
 })
 
 export default router;
