bb3dc51eda0a6947bd95d3d4845f0be58e8c141d
elizabeth pinero <elizabeth.pinero.r@gmail.com>
2025-09-18 18:50:02 +0000

    added documentation to watchlist controller

--- src/controllers/watchlist.ts
+++ src/controllers/watchlist.ts
@@ -10,6 +10,15 @@ async function createWatchlistTableFn() {
     const result = pool.query(createWatchlistTable)
     return result;
 }
+/**
+ * Adds a stock to the watchlist.
+ * @param {string} user_id - The id of the user.
+ * @param {string} symbol - The symbol of the stock.
+ * @param {string} name - The name of the stock.
+ * @param {string} current_price - The current price of the stock.
+ * @param {Sparkline} sparkline - The sparkline of the stock.
+ * @returns {Promise<object[]>} The result of the query.
+ */
 async function addToWatchlistFn(user_id: string, symbol: string, name: string, current_price: string, sparkline: Sparkline) {
     const result = await pool.query(addToWatchlist, [user_id, symbol, name, current_price, JSON.stringify(sparkline)])
     return result.rows
