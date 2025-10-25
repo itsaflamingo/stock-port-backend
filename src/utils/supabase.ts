eb5cb9a57d1665b6b1a58487631c8b0f4816122a
elizabeth pinero <elizabeth.pinero.r@gmail.com>
2025-10-25 13:02:39 +0000

    updated environment variables

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,16 +1,17 @@
 // import { createClient } from '@supabase/supabase-js'
-import { Client } from 'pg';
+// import { Client } from 'pg';
+import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL
-})
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL
+// })
 // Create a single supabase client for interacting with database
-//  const supabase = createClient(
-//   process.env.SUPABASE_URL!,
-//   process.env.SUPABASE_ANON_KEY!
-// );
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+);
 
-// export default supabase;
+export default supabase;
